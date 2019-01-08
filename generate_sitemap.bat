64099dc1adc1947300cc9a1dc5e9489e7037b5aa
Wilson Tian <wilson.tian@perkinelmer.com>
2019-01-08 21:16:06 +0800

    update generate_sitemap.bat

--- generate_sitemap.bat
+++ generate_sitemap.bat
@@ -1,1 +1,1 @@
-node_modules\.bin\sitemap-static --prefix=https://tool.yio.cloud build > sitemap.xml
+node_modules\.bin\sitemap-static --prefix=https://tool.yio.cloud/file-to-base64 --pretty build > sitemap.xml
